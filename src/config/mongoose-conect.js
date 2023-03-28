c9b099f753f2a292f42c907ea99648d3e3e9780d
panupongth148 <97428822+panupongth148@users.noreply.github.com>
2023-03-28 15:12:52 +0700

    edit secure

--- src/config/mongoose-conect.js
+++ src/config/mongoose-conect.js
@@ -1,19 +1,14 @@
 const mongoose = require("mongoose");
-// require('dotenv').config()
+require('dotenv').config()
 
-const uri = "mongodb+srv://cluster0.p3z4ncq.mongodb.net/?retryWrites=true&w=majority";
+
+
+const uri = process.env.MONGO_HOST;
 const options = {
-  dbName: "rightRequest",
-  user: "righthistory",
-  pass: "vyfq8wjMmoHdzSZU",
+  dbName: process.env.MONGO_DB,
+  user: process.env.MONGO_USER,
+  pass: process.env.MONGO_PASS,
 };
-
-// const uri = process.env.MONGO_HOST;
-// const options = {
-//   dbName: process.env.MONGO_DB,
-//   user: process.env.MONGO_USER,
-//   pass: process.env.MONGO_PASS,
-// };
 
 module.exports = mongoose.connect(uri, options);
 
